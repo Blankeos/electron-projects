9279b8a8a6700b9132121e3999a9bb898198c5b6
Blankeos <carloantonioct@gmail.com>
2021-11-24 22:37:47 +0800

    render process properly using mediaRecorder

--- electron-screencap/src/render.js
+++ electron-screencap/src/render.js
@@ -1,16 +1,40 @@
+// Electron API
+const { ipcRenderer } = require("electron");
+
+// Global State
+let mediaRecorder; // MediaRecorder isntance to capture footage
+const recordedChunks = [];
+
 // Buttons
 const videoElement = document.querySelector("video");
 const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const videoSelectBtn = document.getElementById("videoSelectBtn");
-const { ipcRenderer } = require("electron");
 
 videoSelectBtn.onclick = getVideoSources;
+
+startBtn.onclick = (e) => {
+  mediaRecorder.start();
+  startBtn.classList.add("bg-red-500");
+  startBtn.classList.remove("bg-green-500");
+  startBtn.innerText = "Recording";
+};
+
+stopBtn.onclick = (e) => {
+  mediaRecorder.stop();
+  startBtn.classList.remove("bg-red-500");
+  startBtn.classList.add("bg-green-500");
+  startBtn.innerText = "Start";
+};
 
 async function getVideoSources(e) {
   e.preventDefault();
   ipcRenderer.send("show-video-sources-contextmenu");
   console.log("Getting video sources...");
+}
+
+async function startRecording(e) {
+  e.preventDefault();
 }
 
 ipcRenderer.on(
@@ -21,5 +45,33 @@ ipcRenderer.on(
 
     videoElement.srcObject = stream;
     videoElement.play();
+
+    // Create the Media Recorder
+    const options = { mimeType: "video/webm; codecs=vp9" };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    // Register Event Handlers
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
   }
 );
+
+// Captures all recorded chunks
+function handleDataAvailable(e) {
+  console.log("video data available");
+  recordedChunks.push(e.data);
+}
+
+async function handleStop(e) {
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm; codecs=vp9",
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  // console.log(buffer);
+  // Main
+  ipcRenderer.send("on-stop-recording", {
+    buffer,
+  });
+}
